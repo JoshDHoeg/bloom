e09a6c8c36f5a8f60ac98ee89174f6d7e2d95374
Josh Hoeg <josh.hoeg@du.edu>
2019-01-30 12:52:30 -0700

    switched code over from the designer dashboard to the project dashboard

--- src/containers/Designer/index.jsx
+++ src/containers/Designer/index.jsx
@@ -1,10 +1,6 @@
 // BLOOMTIME DESIGN 2019
 import React, { Component } from 'react';
 
-//Youtube video and typeform imports
-import YoutubeEmbedVideo from "youtube-embed-video";
-import { ReactTypeformEmbed } from 'react-typeform-embed';
-
 //IMPORT UTILITIES
 import { withFirebase } from '../../utilities/Firebase';
 import { withAuthorization } from '../../utilities/Session';
@@ -52,7 +48,7 @@ class AdminPage extends Component {
           {loading && <div>Loading ...</div>}
 
           <UserList users={users} />
-          <ClientConcept/>
+
       </div>
     );
   }
@@ -76,13 +72,6 @@ const UserList = ({ users }) => (
   </ul>
 );
 
-const ClientConcept = ({ }) => (
-  <ul>
-    <YoutubeEmbedVideo videoId="ygggcqKmUts" suggestions={false}/>
-    <button type="button"><a href="https://drive.google.com/drive/folders/1H-aSlCfzkodqk8W7JWWv_z8L1GifTZR2?usp=sharing" style={{textDecoration: 'none', color: "black"}}>Google Drive Folder</a></button>
-   {/* <ReactTypeformEmbed id="form" popup={true} autoOpen={true} url="https://demo.typeform.com/to/njdbt5" /> */}
-  </ul>
-);
 
 const condition = authUser => !!authUser;
 
--- src/containers/Project/index.jsx
+++ src/containers/Project/index.jsx
@@ -1,6 +1,10 @@
 // BLOOMTIME DESIGN 2019
 import React from 'react';
 
+//Youtube video and typeform imports
+import YoutubeEmbedVideo from "youtube-embed-video";
+import { ReactTypeformEmbed } from 'react-typeform-embed';
+
 //IMPROT UTILITIES
 import { withAuthorization } from '../../utilities/Session';
 
@@ -8,8 +12,18 @@ const HomePage = () => (
   <div>
     <h1>Home Page</h1>
     <p>The Home Page is accessible by every signed in user.</p>
+    <ClientConcept/>
   </div>
 );
+
+const ClientConcept = ({ }) => (
+  <ul>
+    <YoutubeEmbedVideo videoId="ygggcqKmUts" suggestions={false}/>
+    <button type="button"><a href="https://drive.google.com/drive/folders/1H-aSlCfzkodqk8W7JWWv_z8L1GifTZR2?usp=sharing" style={{textDecoration: 'none', color: "black"}}>Google Drive Folder</a></button>
+   {/* <ReactTypeformEmbed id="form" popup={true} autoOpen={true} url="https://demo.typeform.com/to/njdbt5" /> */}
+  </ul>
+);
+
 
 const condition = authUser => !!authUser;
 
