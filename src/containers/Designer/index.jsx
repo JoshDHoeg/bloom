87787edf5c9e8a9f1c1480a722c2e08a8b0cf7a0
Taylor Danielewski <taylor.danielewski@du.edu>
2019-01-30 12:34:30 -0700

    React (CLIENT) - Concept
    Added youtube embed, Button to google doc folder, Typeform embed (Commented out) on the designer page, didnt know where to put them

--- src/containers/Designer/index.jsx
+++ src/containers/Designer/index.jsx
@@ -1,9 +1,14 @@
 // BLOOMTIME DESIGN 2019
 import React, { Component } from 'react';
 
+//Youtube video and typeform imports
+import YoutubeEmbedVideo from "youtube-embed-video";
+import { ReactTypeformEmbed } from 'react-typeform-embed';
+
 //IMPORT UTILITIES
 import { withFirebase } from '../../utilities/Firebase';
 import { withAuthorization } from '../../utilities/Session';
+import { format } from 'path';
 
 class AdminPage extends Component {
   constructor(props) {
@@ -47,6 +52,7 @@ class AdminPage extends Component {
           {loading && <div>Loading ...</div>}
 
           <UserList users={users} />
+          <ClientConcept/>
       </div>
     );
   }
@@ -67,6 +73,14 @@ const UserList = ({ users }) => (
         </span>
       </li>
     ))}
+  </ul>
+);
+
+const ClientConcept = ({ }) => (
+  <ul>
+    <YoutubeEmbedVideo videoId="ygggcqKmUts" suggestions={false}/>
+    <button type="button"><a href="https://drive.google.com/drive/folders/1H-aSlCfzkodqk8W7JWWv_z8L1GifTZR2?usp=sharing" style={{textDecoration: 'none', color: "black"}}>Google Drive Folder</a></button>
+   {/* <ReactTypeformEmbed id="form" popup={true} autoOpen={true} url="https://demo.typeform.com/to/njdbt5" /> */}
   </ul>
 );
 
