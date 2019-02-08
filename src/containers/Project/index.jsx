910ae7e68413f400c71cc4750b5d4c14b3273fba
Taylor Danielewski <Taylor.Danielewski@du.edu>
2019-02-07 19:30:41 -0700

    bug fix

--- src/containers/Project/index.jsx
+++ src/containers/Project/index.jsx
@@ -14,7 +14,7 @@ import FinalPage from './Final';
 import RevisionsPage from './Revisions';
 
 const HomePageWithSideBar = () => (
-        <Sidebar.Pushable as={Segment} style={{ marginTop: "-9px", marginLeft: '-3px', height: "290px" }}>
+        <Sidebar.Pushable as={Segment} style={{ marginTop: "-9px", marginLeft: '-3px', minHeight: "290px" }}>
             <Router>
                 <div>
                     <Sidebar as={Menu} icon='labeled' inverted vertical visible width='thin'>
