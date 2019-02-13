15759a051cb8a88d49bf9dae806411698e4fadf1
Taylor Danielewski <Taylor.Danielewski@du.edu>
2019-02-13 14:29:21 -0700

    Beginning to pull from Database

--- src/containers/Project/Brief/index.jsx
+++ src/containers/Project/Brief/index.jsx
@@ -7,76 +7,93 @@ import { withAuthorization } from '../../../utilities/Session';
 import tempLogo from '../../../Images/TempLogo.JPG';
 
 class ClientDesignBrief extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: false,
+            user: "t",
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ loading: true });
+        this.user = this.props.firebase.user('userAuthID').then(user => {
+            this.setState({
+                user: user,
+            });
+        });
+    }
+
 
     render() {
-
-        const ColoredLine = ({ }) => (
+        const { user } = this.state;
+        const ClientName = user.name;
+    
+        const ColoredLine = ({}) => (
             <hr
-                style={{
-                    color: 'black',
-                    backgroundColor: 'black',
-                    height: 1,
-                    bottom: 0,
-                }}
-            />
-        );
-
+                    style={{
+                        color: 'black',
+                        backgroundColor: 'black',
+                        height: 1,
+                        bottom: 0,
+                    }}
+                />
+                );
+        
         const MailFunction = () => {
-
-            var ClientName = "[Client Name]";
             var Address = "[Client Address]";
-            var email = "mailto:info@bloomtimedesign.co?subject=" + { ClientName } + '\'s Design Brief at ' + { Address };
+                var email = "mailto:info@bloomtimedesign.co?subject=" + ClientName + '\'s Design Brief at ' + Address;
             return <a href={email}> Here</a>
 
-        }
-
+                }
+        
         const ProjectHeader = () => {
-            var ClientName = "[Client Name]";
             var Address = "[Client Address]";
-            return <h3>{ClientName} Project at {Address}</h3>;
-        }
-
+            return <h3>{ClientName}'s Project at {Address}</h3>;
+            }
+    
         const GoalList = () => {
             var goalOne = "Goal 1";
-            var goalTwo = "Goal 2";
-            var goalThree = "Goal 3";
-            return (
+                var goalTwo = "Goal 2";
+                var goalThree = "Goal 3";
+                return (
                 <ul>
                     <li>{goalOne}</li>
                     <li>{goalTwo}</li>
                     <li>{goalThree}</li>
                 </ul>
-            )
-        };
-
-
+                )
+            };
+    
+    
         const DetailList = () => {
             var PropertyLocation = "[Location on property]";
-            var BudgetRange = "[budget range]";
-            var GoogleMapsURL = "https://www.google.com/maps";
-            return (
+                var BudgetRange = "[budget range]";
+                var GoogleMapsURL = "https://www.google.com/maps";
+                return (
                 <ul>
                     <li>Located on the {PropertyLocation}<br />See it on <a href={GoogleMapsURL}>Google Maps</a></li>
                     <br></br> {/*Temp break until the gap is styled with css*/}
                     <li>Budget: {BudgetRange}</li>
                 </ul>
-            )
-        }
-
+                )
+            }
+    
         var MediaList = () => {
             var GoogleDocsURL = "https://drive.google.com/drive/folders/1H-aSlCfzkodqk8W7JWWv_z8L1GifTZR2?usp=sharing";
-            return (
+                return (
                 <a href={GoogleDocsURL}>Click here for site plan, sun map, photos, etc.</a>
-            );
-        }
-
+                );
+            }
+    
         var Narrative = () => {
             var NarrativeText = "[Narrative Text]";
-            return (
+                return (
                 <p>{NarrativeText}</p>
-            );
-        }
-
+                );
+            }
+    
         var TasteProfile = () => {
 
             return (
@@ -122,54 +139,54 @@ class ClientDesignBrief extends Component {
                         <label style={{ paddingRight: "10px" }}> Climbing</label>
                     </li>
                 </div>
-            );
-
-        }
-
-        return (
-
+                );
+    
+            }
+    
+            return (
+    
             <div class="ui stackable grid container" style={{ paddingTop: "40px", float: 'left', height: "100vh" }}>
-                <div style={{ paddingLeft: '40px' }}>
-                    <ProjectHeader />
-                    <div style={{ float: 'right' }}>
-                        <img src={tempLogo} />
-                    </div>
-                    <h1>Bloomtime Design Brief</h1>
-                    <div style={{ width: '600px' }}>
-                        <div style={{ float: 'left' }}>
-                            <h3>Goals</h3>
-                            <GoalList />
-                        </div>
+                    <div style={{ paddingLeft: '40px' }}>
+                        <ProjectHeader />
                         <div style={{ float: 'right' }}>
-                            <h3>Details</h3>
-                            <DetailList />
-                            <h3>Media</h3>
-                            <MediaList />
+                            <img src={tempLogo} />
                         </div>
-                    </div>
-                    <div style={{ float: 'left', width: '600px' }}>
-                        <ColoredLine />
+                        <h1>Bloomtime Design Brief</h1>
                         <div style={{ width: '600px' }}>
                             <div style={{ float: 'left' }}>
-                                <h3>Narrative</h3>
-                                <Narrative />
+                                <h3>Goals</h3>
+                                <GoalList />
                             </div>
                             <div style={{ float: 'right' }}>
-                                <h3>Taste Profile</h3>
-                                <TasteProfile />
-                                <div style={{ paddingTop: 200 }}>
-                                    <h3>Comments? Click <MailFunction /></h3>
+                                <h3>Details</h3>
+                                <DetailList />
+                                <h3>Media</h3>
+                                <MediaList />
+                            </div>
+                        </div>
+                        <div style={{ float: 'left', width: '600px' }}>
+                            <ColoredLine />
+                            <div style={{ width: '600px' }}>
+                                <div style={{ float: 'left' }}>
+                                    <h3>Narrative</h3>
+                                    <Narrative />
+                                </div>
+                                <div style={{ float: 'right' }}>
+                                    <h3>Taste Profile</h3>
+                                    <TasteProfile />
+                                    <div style={{ paddingTop: 200 }}>
+                                        <h3>Comments? Click <MailFunction /></h3>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-        )
-    }
-}
-
-const condition = authUser => !!authUser;
-
-export default withAuthorization(condition)(ClientDesignBrief);
+                )
+            }
+        }
+        
+        const condition = authUser => !!authUser;
+        
+        export default withAuthorization(condition)(ClientDesignBrief);
