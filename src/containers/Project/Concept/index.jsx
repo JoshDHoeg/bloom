a4f60232b2b74fe3abbd924335299a0e19781a28
Taylor Danielewski <Taylor.Danielewski@du.edu>
2019-02-23 19:24:21 -0700

    Concept, Final, Revision now follow the wireframe
    Made changes Concept, Final, and Revision to mirror the wireframe nearly perfectly. Just need to figure out how to bold the header. She lookin guuud

--- src/containers/Project/Concept/index.jsx
+++ src/containers/Project/Concept/index.jsx
@@ -13,20 +13,23 @@ import { withAuthorization } from '../../../utilities/Session';
 const ClientConcept = () => (
     <div style={{ backgroundImage: "url(" + backgroundTemp + ")", backgroundRepeat: 'repeat' }}>
         <div class="ui stackable grid container">
+            <div class="row" style={{ paddingTop: "40px" }}>
+                <h1>20% Concept Design</h1>
+                <button type="button" style={{ backgroundColor: "#56CCF2", marginLeft: "242px", width: "100px", height: "40px", borderRadius: "4px", border: "#56CCF2", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.1)" }}><a href="https://drive.google.com/drive/folders/1H-aSlCfzkodqk8W7JWWv_z8L1GifTZR2?usp=sharing" style={{ textDecoration: 'none', color: "white" }}>Media</a></button>
+            </div>
             <div class="row">
-                <span id="test" style={{ backgroundColor: "white"}}>
-                    <h1 style={{backgroundColor: "#27AE60", color: "white", textAlign: "center", fontSize: "15px", padding: "10px"}}>The Design</h1>
+                <span style={{ backgroundColor: "white", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.2)", borderRadius: "4px" }}>
+                    <h1 style={{ backgroundColor: "#2F80ED", color: "white", textAlign: "center", fontSize: "15px", padding: "10px", borderTopLeftRadius: "4px", borderTopRightRadius: "4px" }}>Video Explanation</h1>
                     <YoutubeEmbedVideo videoId="ygggcqKmUts" suggestions={false} style={{ width: "600px", padding: "30px" }} />
                 </span>
             </div>
-            <div class="row" style={{ height: 400 }}>
-                {/*Typeform being a bitch again*/}
-                <span style={{ backgroundColor: "white", padding: "30px", width: "600px" }}>
-                    <ReactTypeformEmbed popup={false} url="https://demo.typeform.com/to/njdbt5" style={{ width: 'inherit' }} />
-                </span>
-            </div>
             <div class="row">
-                <button type="button"><a href="https://drive.google.com/drive/folders/1H-aSlCfzkodqk8W7JWWv_z8L1GifTZR2?usp=sharing" style={{ textDecoration: 'none', color: "black" }}>Google Drive Folder</a></button>
+                {/*Typeform being a bitch again*/}
+                <span style={{ backgroundColor: "white", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.2)", borderRadius: "4px" }}>
+                    <h1 style={{ backgroundColor: "#F2994A", color: "white", textAlign: "center", fontSize: "15px", paddingTop: "10px", paddingBottom: "10px", borderTopLeftRadius: "4px", borderTopRightRadius: "4px" }}>Feedback</h1>
+                    <ReactTypeformEmbed popup={false} url="https://demo.typeform.com/to/njdbt5" style={{ width: "600px", height: "375px", padding: "30px", paddingTop: "90px" }} />
+                    <YoutubeEmbedVideo suggestions={false} style={{ width: "600px", padding: "30px", visibility: "hidden" }} />
+                </span>
             </div>
         </div>
     </div>
--- src/containers/Project/Final/index.jsx
+++ src/containers/Project/Final/index.jsx
@@ -15,22 +15,33 @@ import { withAuthorization } from '../../../utilities/Session';
 
 const ClientFinal = () => (
     <div style={{ backgroundImage: "url(" + backgroundTemp + ")", backgroundRepeat: 'repeat' }}>
-    <div class="ui stackable grid container" >
-        <div class="row">
-
-            <FigmaEmbed url="https://www.figma.com/file/LKQ4FJ4bTnCSjedbRpk931/Sample-File" />
-        </div>
-        <div class="row">
-            <YoutubeEmbedVideo videoId="3YG4h5GbTqU" suggestions={false} />
-        </div>
-        <div class="row">
-            <button type="button"><a href="https://drive.google.com/drive/folders/1H-aSlCfzkodqk8W7JWWv_z8L1GifTZR2?usp=sharing" style={{ textDecoration: 'none', color: "black" }}>Google Drive Folder</a></button>
-        </div>
-        <div class="row" style={{ height: 400 }}>
-            <ReactTypeformEmbed popup={false} url="https://demo.typeform.com/to/njdbt5" />
-        </div>
-    </div >
-</div>
+        <div class="ui stackable grid container" >
+        <div class="row" style={{ paddingTop: "40px" }}>
+                <h1>Rough Draft</h1>
+                <button type="button" style={{ backgroundColor: "#56CCF2", marginLeft: "345px", width: "100px", height: "40px", borderRadius: "4px", border: "#56CCF2", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.1)" }}><a href="https://drive.google.com/drive/folders/1H-aSlCfzkodqk8W7JWWv_z8L1GifTZR2?usp=sharing" style={{ textDecoration: 'none', color: "white" }}>Media</a></button>
+            </div>
+            <div class="row">
+            <span style={{ backgroundColor: "white", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.2)", borderRadius: "4px" }}>
+                    <h1 style={{ backgroundColor: "#27AE60", color: "white", textAlign: "center", fontSize: "15px", padding: "10px", borderTopLeftRadius: "4px", borderTopRightRadius: "4px" }}>The Design</h1>
+                <FigmaEmbed url="https://www.figma.com/file/LKQ4FJ4bTnCSjedbRpk931/Sample-File" style={{ width: "540px", margin: "30px" }}/>
+                </span>
+            </div>
+            <div class="row">
+                <span style={{ backgroundColor: "white", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.2)", borderRadius: "4px" }}>
+                    <h1 style={{ backgroundColor: "#2F80ED", color: "white", textAlign: "center", fontSize: "15px", padding: "10px", borderTopLeftRadius: "4px", borderTopRightRadius: "4px" }}>Video Explanation</h1>
+                    <YoutubeEmbedVideo videoId="3YG4h5GbTqU" suggestions={false} style={{ width: "600px", padding: "30px" }} />
+                </span>
+            </div>
+            <div class="row">
+                {/*Typeform being a bitch again*/}
+                <span style={{ backgroundColor: "white", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.2)", borderRadius: "4px" }}>
+                    <h1 style={{ backgroundColor: "#F2994A", color: "white", textAlign: "center", fontSize: "15px", paddingTop: "10px", paddingBottom: "10px", borderTopLeftRadius: "4px", borderTopRightRadius: "4px" }}>Feedback</h1>
+                    <ReactTypeformEmbed popup={false} url="https://demo.typeform.com/to/njdbt5" style={{ width: "600px", height: "375px", padding: "30px", paddingTop: "90px" }} />
+                    <YoutubeEmbedVideo suggestions={false} style={{ width: "600px", padding: "30px", visibility: "hidden" }} />
+                </span>
+            </div>
+        </div >
+    </div>
 );
 
 
