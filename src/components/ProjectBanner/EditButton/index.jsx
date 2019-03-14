3341ca48ee5daec655be23f3eadd973a28203630
Josh Hoeg <josh.hoeg@du.edu>
2019-03-14 01:42:02 -0600

    Almost got the auth

---
+++ src/components/ProjectBanner/EditButton/index.jsx
@@ -0,0 +1,23 @@
+// BLOOMTIME DESIGN 2019
+import React from 'react';
+import { withAuthorization } from '../../../utilities/Session';
+import {Link} from "react-router-dom";
+import * as ROUTES from "../../../utilities/constants/routes";
+
+const EditButton = (props) => (
+    props.edit ? (
+        <button type="button" style={{ backgroundColor: "#27AE60", marginLeft: "260px", width: "100px", height: "40px", borderRadius: "4px", border: "#56CCF2", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.1)" }}><Link to={ROUTES.CLIENT_BRIEF} style={{ textDecoration: 'none', color: "white" }} >Done</Link></button>
+    ) : (
+        <button type="button" style={{ backgroundColor: "#27AE60", marginLeft: "260px", width: "100px", height: "40px", borderRadius: "4px", border: "#56CCF2", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.1)" }}><Link to={ROUTES.CLIENT_BRIEF_EDIT} style={{ textDecoration: 'none', color: "white" }} >Edit</Link></button>
+    )
+);
+
+//this way gets the button to dissappear
+const condition = authUser =>
+  authUser && authUser._isDesigner;
+
+//button is visible
+// const condition = authUser =>
+//     (authUser || authUser._isDesigner);
+
+export default withAuthorization(condition)(EditButton);
--- src/components/ProjectBanner/index.jsx
+++ src/components/ProjectBanner/index.jsx
@@ -1,23 +1,17 @@
 // BLOOMTIME DESIGN 2019
 import React from 'react';
 import { withAuthorization } from '../../utilities/Session';
-import {Link} from "react-router-dom";
-import * as ROUTES from "../../utilities/constants/routes";
+import EditButton from "./EditButton";
 
 const ProjectBanner = (props) => (
     <div className="row" style={{ paddingTop: "40px" }}>
         <h1>Design Brief</h1>
-        {props.edit ? (
-          <button type="button" style={{ backgroundColor: "#27AE60", marginLeft: "260px", width: "100px", height: "40px", borderRadius: "4px", border: "#56CCF2", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.1)" }}><Link to={ROUTES.CLIENT_BRIEF} style={{ textDecoration: 'none', color: "white" }} >Done</Link></button>
-        ) : (
-          <button type="button" style={{ backgroundColor: "#27AE60", marginLeft: "260px", width: "100px", height: "40px", borderRadius: "4px", border: "#56CCF2", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.1)" }}><Link to={ROUTES.CLIENT_BRIEF_EDIT} style={{ textDecoration: 'none', color: "white" }} >Edit</Link></button>
-        )}
+        <EditButton edit={props.edit} />
         <button type="button" style={{ backgroundColor: "#56CCF2", marginLeft: "20px", width: "100px", height: "40px", borderRadius: "4px", border: "#56CCF2", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.1)" }}><a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/drive/folders/1H-aSlCfzkodqk8W7JWWv_z8L1GifTZR2?usp=sharing" style={{ textDecoration: 'none', color: "white" }}>Media</a></button>
     </div>
 );
 
 
-const condition = authUser =>
-  authUser && !authUser._isDesigner;
+const condition = authUser => !!authUser;
 
 export default withAuthorization(condition)(ProjectBanner);
