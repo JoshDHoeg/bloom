15363dc21c7e8d73132d3cb169ff92bc661be4ca
meierwerthan <meier.werthan@du.edu>
2019-04-20 14:01:01 -0600

    ....again

--- src/components/PaymentStripe/backend/constants/frontend.js
+++ src/components/PaymentStripe/backend/constants/frontend.js
@@ -3,8 +3,8 @@
 const FRONTEND_DEV_URLS = [ 'http://localhost:3000' ]; //set the development url
 
 const FRONTEND_PROD_URLS = [ //set the production url
-  'https://www.bloom-test.herokuapp.com/project/final',
-  'https://bloom-test.herokuapp.com/project/final'
+  'https://www.bloom-test.herokuapp.com',
+  'https://bloom-test.herokuapp.com'
 ];
 
 module.exports = process.env.NODE_ENV === 'production' //export constant urls
