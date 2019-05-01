c93f2ac10319a4bbda5744013025ded899d7e16c
Josh Hoeg <josh.hoeg@du.edu>
2019-04-30 19:08:56 -0600

    lots of stuff

--- src/containers/Project/Brief/Components/TasteProfile/TasteProfile.jsx
+++ src/containers/Project/Brief/Components/TasteProfile/TasteProfile.jsx
@@ -1,109 +1,54 @@
 // BLOOMTIME DESIGN 2019
 import React from 'react';
+import { Input, Container } from 'semantic-ui-react';
+
 
 const TasteProfile = (props) => (
-    <div>
-        {props.edit ? (
-        <TasteProfileEdit />
-        ) : (
-        <TasteProfileView />
-        )}
-    </div>
+        <div>
+            {props.edit ? (
+            <TasteProfileEdit brief={props.brief} handleChangeProfile={props.handleChangeProfile}/>
+            ) : (
+            <TasteProfileView brief={props.brief} />
+            )}
+        </div>
 );
 
-const TasteProfileView = () => {
+const TasteProfileView = (props) => {
     return (
-        <div style={{ textAlign: "center" }}>
-        <div style={{ fontSize: 10 }}>
-            <li style={{ fontSize: 13, listStyle: 'none' }}>
-                <h5>Spacing</h5>
-                <p>Wide &nbsp; | &nbsp; Medium &nbsp; | &nbsp; Full &nbsp; | &nbsp; Lush
-        </p>
+        <Container>
+            <div>{props.brief.profile.spacing}</div>
 
-                <h5>Spacing</h5>
-                <p>Repetitive &nbsp; | &nbsp; Massings &nbsp; | &nbsp; Groups &nbsp; | &nbsp; Mixed
-        </p>
+            <div>{props.brief.profile.variety}</div>
 
-                <h5>Spacing</h5>
-                <p>V. Straight &nbsp; | &nbsp; Straight &nbsp; | &nbsp; Curious &nbsp; | &nbsp; V. Curved
-        </p>
-            </li>
-            <li style={{ paddingTop: 20, listStyle: 'none' }}>
-                <h5>Ground Cover</h5>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Rocks &amp; Stone</label>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Mulch</label>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Spreading</label>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Mix</label>
-            </li>
+            <div>{props.brief.profile.edging}</div>
 
-            <li style={{ paddingTop: 20, flexWrap: 'wrap', listStyle: 'none' }}>
-                <h5>Plant Form</h5>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Flat &amp; Spreading</label>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Small</label>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Medium/Upright</label>
-                <br /> {/*Temp break until styling*/}
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Shrubs &amp; Hedges</label>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Climbing</label>
-            </li>
-        </div>
-    </div>
+            <div>{props.brief.profile.ground}</div>
+
+            <div>{props.brief.profile.form}</div>
+        </Container>
     )
 }
 
-const TasteProfileEdit = () => {
+const TasteProfileEdit = (props) => {
+    console.log(props);
     return (
-        <div style={{ textAlign: "center" }}>
-        <div style={{ fontSize: 10 }}>
-            <li style={{ fontSize: 13, listStyle: 'none' }}>
-                <h5>Spacing</h5>
-                <p>Wide &nbsp; | &nbsp; Medium &nbsp; | &nbsp; Full &nbsp; | &nbsp; Lush
-        </p>
+        <Container>
+            <label id="SpacingEdit">Spacing</label>
+            <Input name="spacing" value={props.brief.profile.spacing} onChange={props.handleChangeProfile}/>
 
-                <h5>Spacing</h5>
-                <p>Repetitive &nbsp; | &nbsp; Massings &nbsp; | &nbsp; Groups &nbsp; | &nbsp; Mixed
-        </p>
+            <label id="VarietyEdit">Variety</label>
+            <Input name="variety" value={props.brief.profile.variety} onChange={props.handleChangeProfile}/>
 
-                <h5>Spacing</h5>
-                <p>V. Straight &nbsp; | &nbsp; Straight &nbsp; | &nbsp; Curious &nbsp; | &nbsp; V. Curved
-        </p>
-            </li>
-            <li style={{ paddingTop: 20, listStyle: 'none' }}>
-                <h5>Ground Cover</h5>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Rocks &amp; Stone</label>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Mulch</label>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Spreading</label>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Mix</label>
-            </li>
+            <label id="EdgingEdit">Edging</label>
+            <Input name="edging" value={props.brief.profile.edging} onChange={props.handleChangeProfile}/>
 
-            <li style={{ paddingTop: 20, flexWrap: 'wrap', listStyle: 'none' }}>
-                <h5>Plant Form</h5>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Flat &amp; Spreading</label>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Small</label>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Medium/Upright</label>
-                <br /> {/*Temp break until styling*/}
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Shrubs &amp; Hedges</label>
-                <input type="checkbox"></input>
-                <label style={{ paddingRight: "10px" }}> Climbing</label>
-            </li>
-        </div>
-    </div>
+            <label id="GroundEdit">Ground Cover</label>
+            <Input name="ground" value={props.brief.profile.ground} onChange={props.handleChangeProfile}/>
+
+            <label id="FormEdit">Plant Form</label>
+            <Input name="form" value={props.brief.profile.form} onChange={props.handleChangeProfile}/>
+        </Container>
+        
     )
 }
 
--- src/containers/Project/Brief/View/View.jsx
+++ src/containers/Project/Brief/View/View.jsx
@@ -57,7 +57,7 @@ class BriefPageView extends Component {
 
                             <span style={{ width: "600px", backgroundColor: "white", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.2)", borderRadius: "4px" }}>
                                 <h1 style={{ backgroundColor: "#27AE60", color: "white", textAlign: "center", fontSize: "15px", paddingTop: "10px", paddingBottom: "10px", borderTopLeftRadius: "4px", borderTopRightRadius: "4px" }}>Taste Profile</h1>
-                                <TasteProfile edit={this.state.edit}/>
+                                <TasteProfile edit={this.state.edit} brief={this.props.brief}/>
                             </span>
                         </div>
                     </div>
