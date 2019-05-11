28f76f69a8c3e940ad30ebb3febc46da6987c007
meierwerthan <meier.werthan@du.edu>
2019-05-10 22:24:57 -0600

    removed messaging from nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -27,9 +27,6 @@ const NavigationAuth = () => (
             <div className="item">
                 <Link to={ROUTES.ACCOUNT_INFO}>Account</Link>
             </div>
-            <div className="item">
-                <Link to={ROUTES.MESSAGING}>Messages</Link>
-            </div>
             <div className="right menu item">
                 <h3>Bloomtime </h3>
             </div>
