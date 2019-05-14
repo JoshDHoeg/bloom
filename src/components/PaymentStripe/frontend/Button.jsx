c204bb89d1e45b30e583aa1d8c101e6c40107f7c
Grace <gkorniczky@outlook.com>
2019-05-13 19:39:25 -0600

    checkout form or button changed

--- src/components/PaymentStripe/frontend/Button.jsx
+++ src/components/PaymentStripe/frontend/Button.jsx
@@ -3,20 +3,37 @@
 import React,{Component} from 'react';
 import Checkout from './Checkout';
 import logo from './../../../Images/TempLogo.JPG';
+import {CardElement, injectStripe} from 'react-stripe-elements';
 
-const Button = (props) => {
-  console.log(props);
-  return(
-    <Checkout //initiate Checkout.js and configure
-    label={'Purchase Final Design'}
-    name={'Purchase Your Final Design'}
-    description={'$599 per area up to 1,000 sq. feet'}
-    amount={599.00}
-    image ={logo}
-    billingAddress={true}
-    SuccessPayment = {props.SuccessPayment}
-    />  
-    )
+class CheckoutForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {complete: false};
+    this.submit = this.submit.bind(this);
   }
 
-export default (Button)
+  async submit(ev) {
+    let {token} = await this.props.stripe.createToken({name: "Name"});
+    let response = await fetch("/charge", {
+      method: "POST",
+      headers: {"Content-Type": "text/plain"},
+      body: token.id
+    });
+  
+    if (response.ok) this.setState({complete: true});
+  }
+
+  render() {
+    if (this.state.complete) return <h1>Purchase Complete</h1>;
+
+    return (
+      <div className="checkout">
+        <p>Would you like to complete the purchase?</p>
+        <CardElement />
+        <button onClick={this.submit}>Send</button>
+      </div>
+    );
+  }
+}
+
+export default injectStripe(CheckoutForm);
