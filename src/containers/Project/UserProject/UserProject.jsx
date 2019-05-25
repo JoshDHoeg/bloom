58ab046bcc2a977594f76d98f95620d180a3f8e8
Jesse Ruder-Hook <jesse.ruderhook@gmail.com>
2019-05-24 20:04:46 -0600

    added new project routes for new ui

--- src/containers/Project/UserProject/UserProject.jsx
+++ src/containers/Project/UserProject/UserProject.jsx
@@ -1,12 +1,51 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Icon, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
 //IMPROT UTILITIES
 import { withAuthorization } from '../../../utilities/Session';
+import * as ROUTES from "../../../utilities/constants/routes";
+import BriefPage from "../DesignerProject/Brief/Brief";
 
+import Concept from './Concept/Concept';
+import Contractors from './Contractors/Contractors';
+import Final from './Final/Final';
+import Revision from './Revision/Revision';
+import Draft from './Draft/Draft';
+
+
+//concept => draft => final => revision => contractors
 class UserProject extends React.Component {
     render(){
         return(
-            <div> This is the user proj component </div>
+            <Router>
+                <Segment basic>
+                    <div>
+                        <Route exact path={ROUTES.CONCEPT} component={Concept} />
+                        <Route exact
+                               path={ROUTES.CONCEPT}
+                               render={(props) => <Concept {...props} edit={false} index={props.location.state} /> }
+                        />
+                        <Route exact
+                               path={ROUTES.CONTRACTORS}
+                               render={(props) => <Contractors {...props} edit={false} /> }
+                        />
+                        <Route exact
+                               path={ROUTES.FINAL}
+                               render={(props) => <Final {...props} edit={false} /> }
+                        />
+                        <Route exact
+                               path={ROUTES.REVISION}
+                               render={(props) => <Revision {...props} edit={false} /> }
+                        />
+                        <Route exact
+                               path={ROUTES.DRAFT}
+                               render={(props) => <Draft {...props} edit={false} /> }
+                        />
+                    </div>
+                    <div> This is the user proj component </div>
+                </Segment>
+            </Router>
         )
     }
 }
