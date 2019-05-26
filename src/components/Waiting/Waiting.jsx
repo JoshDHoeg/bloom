c7c015498e2295b1ef352f6f825aeeeb0f7211a4
meierwerthan <meier.werthan@du.edu>
2019-05-25 20:34:20 -0600

    added button for waiting page

--- src/components/Waiting/Waiting.jsx
+++ src/components/Waiting/Waiting.jsx
@@ -3,6 +3,12 @@ import {withAuthorization} from '../../utilities/Session'
 import logo from '../../Images/TempLogo.JPG';
 import { Container, Grid } from 'semantic-ui-react'
 import ProjectStatus from '../ProjectStatus/ProjectStatus';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+library.add(faArrowRight);
+library.add(faArrowLeft);
 
 class Waiting extends Component {
     constructor(props){
@@ -13,6 +19,7 @@ class Waiting extends Component {
             title: '',
             src: '',
             message: '',
+            link: ''
         }
     }
     
@@ -21,48 +28,56 @@ class Waiting extends Component {
             this.setState({
                 title: 'Contractors',
                 src: logo,
-                message: 'Your contractors havent gotten us their quotes yet. You will receive a notification when they are ready'
+                message: 'Your contractors havent gotten us their quotes yet. You will receive a notification when they are ready',
+                link: '/project/user_revision'
             })
         }
         else if(this.props.state === 'draft'){
             this.setState({
                 title: 'Draft',
                 src: logo,
-                message: 'Your Draft is not ready yet. You will receive a notification when it is ready'
+                message: 'Your Draft is not ready yet. You will receive a notification when it is ready',
+                link: '/project/user_concept'
             })
         }
         else if(this.props.state === 'final'){
             this.setState({
                 title: 'Final',
                 src: logo,
-                message: 'Your Final is not ready yet. You will receive a notification when it is ready'
+                message: 'Your Final is not ready yet. You will receive a notification when it is ready',
+                link: '/project/user_draft'
             })
         }
         else if(this.props.state === 'revision'){
             this.setState({
                 title: 'Revision',
                 src: logo,
-                message: 'Your Revision is not ready yet. You will receive a notification when it is ready'
+                message: 'Your Revision is not ready yet. You will receive a notification when it is ready',
+                link: '/project/user_final'
             })
         }
         else{
             this.setState({
                 title: 'Concept',
                 src: logo,
-                message: 'Your Concept is not ready yet. You will receive a notification when it is ready'
+                message: 'Your Concept is not ready yet. You will receive a notification when it is ready',
             })
         }
     }
 
 
     render(){
+        console.log(this.state.link)
         return(
             <Container>
                 <Grid.Row>
                     <ProjectStatus state={this.props.state}/>
                 </Grid.Row>
+                <Link to={this.state.link} style={{ position: "absolute", right: "90%", top: "250px" }}>
+                            <FontAwesomeIcon icon="arrow-left" size="5x" color="black" />
+                        </Link>
                 <Grid.Row>
-                    <Grid.Column style={{textAlign: 'center', paddingBottom: '15px'}}>
+                    <Grid.Column style={{textAlign: 'center', paddingBottom: '15px', paddingTop: '15px'}}>
                         <div style={{fontSize:'30px', fontWeight:'bold'}}>{this.state.title}</div>
                     </Grid.Column>
                     <Grid.Column style={{textAlign: 'center',paddingBottom: '15px'}}>
--- src/containers/Account/components/AccountInfo/View/View.jsx
+++ src/containers/Account/components/AccountInfo/View/View.jsx
@@ -19,44 +19,42 @@ class AccountInfoPageView extends Component {
     render() {
         console.log(this.props.user);
         return (
-            <div style = {{ backgroundImage: "url(" + backgroundTemp + ")", backgroundRepeat: 'repeat', marginRight:'-14px', marginLeft: "-14px", paddingleft: "14px", paddingBottom: "100vh"}}>
-                <Container>
-                    <Grid textAlign='center'>
-                        <Grid.Row>
-                            <InfoBanner 
+            <Container>
+                <Grid textAlign='center'>
+                    <Grid.Row>
+                        <InfoBanner 
+                        user={this.props.user}
+                        name={this.props.user.name}
+                        />
+                    </Grid.Row>
+                    <Grid.Row>
+                        <span style={{ backGroundColor: "white", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.2)", borderRadius: "4px" }}>
+                            <h1 style={{ backgroundColor: "#4BED2F", color: "white", textAlign: "center", fontSize: "15px", padding: "10px", borderTopLeftRadius: "4px", borderTopRightRadius: "4px" }}>User Information</h1>
+                            <UserInformation 
+                            edit={this.state.edit} 
                             user={this.props.user}
+                            phone={this.props.user.phone}
                             name={this.props.user.name}
                             />
-                        </Grid.Row>
-                        <Grid.Row>
-                            <span style={{ backGroundColor: "white", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.2)", borderRadius: "4px" }}>
-                                <h1 style={{ backgroundColor: "#4BED2F", color: "white", textAlign: "center", fontSize: "15px", padding: "10px", borderTopLeftRadius: "4px", borderTopRightRadius: "4px" }}>User Information</h1>
-                                <UserInformation 
-                                edit={this.state.edit} 
-                                user={this.props.user}
-                                phone={this.props.user.phone}
-                                name={this.props.user.name}
-                                />
-                            </span>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <span style={{ backGroundColor: "white", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.2)", borderRadius: "4px" }}>
-                                <h1 style={{ backgroundColor: "#4BED2F", color: "white", textAlign: "center", fontSize: "15px", padding: "10px", borderTopLeftRadius: "4px", borderTopRightRadius: "4px" }}>Address Information</h1>
-                                <BillingInformation 
-                                edit={this.state.edit}
-                                billadd1={this.props.user.billadd1}
-                                zip={this.props.user.zip}
-                                state={this.props.user.state}
-                                city={this.props.user.city}                            
-                                />
-                            </span>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <EditButton edit={this.state.edit} formSubmit={this.props.formSubmit}/>
-                        </Grid.Row>
-                    </Grid>
-                </Container>
-            </div>
+                        </span>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <span style={{ backGroundColor: "white", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.2)", borderRadius: "4px" }}>
+                            <h1 style={{ backgroundColor: "#4BED2F", color: "white", textAlign: "center", fontSize: "15px", padding: "10px", borderTopLeftRadius: "4px", borderTopRightRadius: "4px" }}>Address Information</h1>
+                            <BillingInformation 
+                            edit={this.state.edit}
+                            billadd1={this.props.user.billadd1}
+                            zip={this.props.user.zip}
+                            state={this.props.user.state}
+                            city={this.props.user.city}                            
+                            />
+                        </span>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <EditButton edit={this.state.edit} formSubmit={this.props.formSubmit}/>
+                    </Grid.Row>
+                </Grid>
+            </Container>
         );
     }
 }
