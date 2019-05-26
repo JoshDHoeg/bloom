ecee400d100730f57451d06b96c9f64d27338367
meierwerthan <meier.werthan@du.edu>
2019-05-26 14:36:33 -0600

    removing production constraint

--- src/components/PaymentStripe/backend/server.js
+++ src/components/PaymentStripe/backend/server.js
@@ -18,9 +18,9 @@ const configureServer = app => { //configure the express server
     extended: true
   }));
   app.use(bodyParser.json());
-  if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-  }
+  // if(process.env.NODE_ENV === 'production'){
+  //   app.use(express.static('client/build'));
+  // }
 };
 
 module.exports = configureServer;
