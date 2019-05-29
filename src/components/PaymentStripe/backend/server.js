4dd23aae7e6eb5977ea647876159f5cdbf282199
meierwerthan <meier.werthan@du.edu>
2019-05-29 10:32:18 -0600

    fixed path

--- src/components/PaymentStripe/backend/server.js
+++ src/components/PaymentStripe/backend/server.js
@@ -2,7 +2,8 @@
 
 const cors = require('cors'); //use cors
 const bodyParser = require('body-parser'); //use body-parser (must also use this dependency)
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
 const CORS_WHITELIST = require('./constants/frontend'); //use cors whitelist to avoid cors header authorization error (must also use this dependency)
 var forceSsl = function (req, res, next) {
