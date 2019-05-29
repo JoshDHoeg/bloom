d900a13ef7e11524e7433f6165be36a914982fe3
meierwerthan <meier.werthan@du.edu>
2019-05-28 21:42:50 -0600

    last try

--- src/components/PaymentStripe/frontend/constants/server.js
+++ src/components/PaymentStripe/frontend/constants/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production' //Setting The back-end server url's
-  ? 'http://bloom-userui.herokuapp.com/'
+  ? 'http://bloom-userui.herokuapp.com/payme'
   : 'http://localhost:8080/';
 
 export default PAYMENT_SERVER_URL;
