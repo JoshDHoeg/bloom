082e4a545bcc5044c66313af380401385878837d
meierwerthan <meier.werthan@du.edu>
2019-05-29 16:41:15 -0600

    why wont node start heroku backend?

--- src/components/PaymentStripe/backend/constants/server.js
+++ src/components/PaymentStripe/backend/constants/server.js
@@ -6,7 +6,7 @@ const SERVER_PORT = 8080; //set the server port
 
 const SERVER_CONFIGS = { //configure the server
   PRODUCTION: process.env.NODE_ENV === 'production',
-  PORT: process.env.PORT || SERVER_PORT,
+  PORT: process.env.NODE_ENV || SERVER_PORT,
 };
 
 module.exports = SERVER_CONFIGS;
