e49ffc567a9c87f66d2ca7d5fd9d512a088b51a6
meierwerthan <meier.werthan@du.edu>
2019-05-28 21:22:19 -0600

    remoed /project

--- src/components/PaymentStripe/backend/constants/frontend.js
+++ src/components/PaymentStripe/backend/constants/frontend.js
@@ -2,7 +2,9 @@
 
 const FRONTEND_DEV_URLS = [ 'http://localhost:3000' ]; //set the development url
 
-const FRONTEND_PROD_URLS = [ 'http://bloom-userui.herokuapp.com/project']; //set the production urls
+const FRONTEND_PROD_URLS = [ 
+  'http://www.bloom-userui.herokuapp.com/',
+  'http://bloom-userui.herokuapp.com/']; //set the production urls
 
 module.exports = process.env.NODE_ENV === 'production' //export constant urls
   ? FRONTEND_PROD_URLS
