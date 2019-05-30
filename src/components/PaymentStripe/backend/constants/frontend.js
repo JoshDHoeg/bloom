4abc129c37eee600807b59d51ee24d2d9d9acdca
meierwerthan <meier.werthan@du.edu>
2019-05-30 14:34:58 -0600

    fixed for https site

--- src/components/PaymentStripe/backend/constants/frontend.js
+++ src/components/PaymentStripe/backend/constants/frontend.js
@@ -3,11 +3,10 @@
 const FRONTEND_DEV_URLS = [ 'http://localhost:3000' ]; //set the development url
 
 const FRONTEND_PROD_URLS = [ 
-  'http://www.bloom-userui.herokuapp.com/',
-  'http://bloom-userui.herokuapp.com/']; //set the production urls
+  'https://www.app.bloomtimedesign.co/',
+  'https://app.bloomtimedesign.co']; //set the production urls
 
 module.exports = process.env.NODE_ENV === 'production' //export constant urls
   ? FRONTEND_PROD_URLS
   : FRONTEND_DEV_URLS;
 
-  
