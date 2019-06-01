50baa60950121dc0a9f5ad3f62992deda4a4eef4
meierwerthan <meier.werthan@du.edu>
2019-06-01 17:55:57 -0600

    https

--- src/components/PaymentStripe/frontend/constants/server.js
+++ src/components/PaymentStripe/frontend/constants/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production' //Setting The back-end server url's
-  ? 'http://app.bloomtimedesign.co/project'
+  ? 'https://bloom-prod.herokuapp.com/'
   : 'http://localhost:8080/';
 
 export default PAYMENT_SERVER_URL;
