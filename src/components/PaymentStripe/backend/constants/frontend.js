d372e83ac1153e9ed987e42daf2aa40266180aef
meierwerthan <meier.werthan@du.edu>
2019-06-05 17:47:03 -0600

    got rid of www.

--- src/components/PaymentStripe/backend/constants/frontend.js
+++ src/components/PaymentStripe/backend/constants/frontend.js
@@ -3,8 +3,8 @@
 const FRONTEND_DEV_URLS = [ 'http://localhost:3000' ]; //set the development url
 
 const FRONTEND_PROD_URLS = [ 
-  'http://www.bloom-userui.herokuapp.com/project',
-  'http://bloom-userui.herokuapp.com/project']; //set the production urls
+  'https://bloom-userui.herokuapp.com/project',
+  'https://bloom-userui.herokuapp.com/project']; //set the production urls
 
 module.exports = process.env.NODE_ENV === 'production' //export constant urls
   ? FRONTEND_PROD_URLS
