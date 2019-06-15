85648b6b9beefb17c394242fab51b4f3945acad3
meierwerthan <meier.werthan@du.edu>
2019-06-15 11:38:02 -0600

    staged for backend test

--- src/components/PaymentStripe/frontend/constants/server.js
+++ src/components/PaymentStripe/frontend/constants/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production' //Setting The back-end server url's
   ? 'https://bloom-expressapi.herokuapp.com/payment'
-  : 'http://localhost:80';
+  : 'http://localhost:8080/payment';
 
 export default PAYMENT_SERVER_URL;
