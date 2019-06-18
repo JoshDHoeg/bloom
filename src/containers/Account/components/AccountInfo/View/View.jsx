e26f9068354bddc2477fe753d16981053c09b755
Josh Hoeg <josh.hoeg@du.edu>
2019-06-18 17:33:53 -0600

    added some styles to the account page

--- src/containers/Account/components/AccountInfo/View/View.jsx
+++ src/containers/Account/components/AccountInfo/View/View.jsx
@@ -28,7 +28,7 @@ class AccountInfoPageView extends Component {
                     <Grid.Row>
                         <Segment>
                         <span style={{ backGroundColor: "white", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.2)", borderRadius: "4px" }}>
-                            <h1 style={{ backgroundColor: "#4BED2F", color: "white", textAlign: "center", fontSize: "15px", padding: "10px", borderTopLeftRadius: "4px", borderTopRightRadius: "4px" }}>User Information</h1>
+                            <h1 style={{ backgroundColor: "#F5BDF9", color: "white", textAlign: "center", fontSize: "15px", padding: "10px", borderTopLeftRadius: "4px", borderTopRightRadius: "4px" }}>User Information</h1>
                             <UserInformation 
                             edit={this.state.edit} 
                             user={this.props.user}
@@ -41,7 +41,7 @@ class AccountInfoPageView extends Component {
                     <Grid.Row>
                         <Segment>
                         <span style={{ backGroundColor: "white", boxShadow: "6px 6px 16px 0px rgba(0,0,0,0.2)", borderRadius: "4px" }}>
-                            <h1 style={{ backgroundColor: "#4BED2F", color: "white", textAlign: "center", fontSize: "15px", padding: "10px", borderTopLeftRadius: "4px", borderTopRightRadius: "4px" }}>Address Information</h1>
+                            <h1 style={{ backgroundColor: "#F5BDF9", color: "white", textAlign: "center", fontSize: "15px", padding: "10px", borderTopLeftRadius: "4px", borderTopRightRadius: "4px" }}>Address Information</h1>
                             <BillingInformation 
                             edit={this.state.edit}
                             billadd1={this.props.user.billadd1}
