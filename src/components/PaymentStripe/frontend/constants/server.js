0504e9552d29647e55abd9ea0ad3f7c12636b73d
meierwerthan <meier.werthan@du.edu>
2019-06-19 16:32:23 -0600

    i fucked up

--- src/components/PaymentStripe/frontend/constants/server.js
+++ src/components/PaymentStripe/frontend/constants/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production' //Setting The back-end server url's
-  ? 'https://app.bloomtimedesign.co'
+  ? ' https://bloom-expressapi.herokuapp.com/'
   : 'http://localhost:8080/payment';
 
 export default PAYMENT_SERVER_URL;
